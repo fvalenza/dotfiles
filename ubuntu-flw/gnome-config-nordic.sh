415da5978aede541ecc36daec843d3a514cc8378
fvalenza <florian.valenza@gmail.com>
2022-12-14 23:57:25 +0100

    Added first version of post-install for ubuntu

---
+++ ubuntu-flw/gnome-config-nordic.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+  exit 1
+fi
+
+username=$(id -u -n 1000)
+builddir=$(pwd)
+develdir=/home/$username/root-filetree/devel
+tmpinstalldir=/home/$username/tmp
+
+run_as_user() {
+  sudo -u $username bash -c "$1";
+}
+
+
+
+# Install fonts
+nala install -y fonts-roboto
+
+# Get Nord theme
+run_as_user "mkdir -p /home/$username/.themes";
+cd /home/$username/.themes
+
+wget https://github.com/EliverLara/Nordic/releases/download/v2.2.0/Nordic-bluish-accent-standard-buttons-v40.tar.xz
+tar -xvf Nordic-bluish-accent-standard-buttons-v40.tar.xz
+# rm -rf Nordic-bluish-accent-standard-buttons-v40.tar.xz
+
+# Get Tela Icons
+# TODO: Mv Icons from repo to /home/$username/.icons
+
+run_as_user "mkdir -p /home/$username/root-filetree/images/artwork/icons";
+cd /home/$username/root-filetree/images/artwork/icons
+run_as_user "git clone https://github.com/vinceliuice/Tela-circle-icon-theme.git";
+cd Tela-circle-icon-theme
+run_as_user "/bin/bash ./install.sh \"-a\"";
+
+# Get Nordic cursor
+run_as_user "mkdir -p /home/$username/.icons";
+tar -xvf ./assets/cursors/Nordic-cursors.tar.xz --directory ./assets/cursors
+run_as_user "cp -R ./assets/cursors/Nordic-cursors /home/$username/.icons";
+
+# Get Wallpaper Cyber city
+run_as_user "cp ./assets/images/aciwnsk876t91.jpg /home/$username/root-filetree/images/artwork/wallpaper";
+
+
+
+# Set fonts
+gsettings set org.gnome.desktop.interface document-font-name 'Roboto 11'
+gsettings set org.gnome.desktop.interface font-name 'Roboto 11'
+gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Roboto Bold 11'
+
+
+# Set Desktop Nord theme
+gsettings set org.gnome.desktop.interface gtk-theme Nordic-bluish-accent-standard-buttons-v40
+gsettings set org.gnome.desktop.wm.preferences theme Nordic-bluish-accent-standard-buttons-v40
+
+
+# Set Tela icons
+gsettings set org.gnome.desktop.interface icon-theme Tela-circle-orange
+
+# Change mouse cursor ?
+# TODO: Mv Cursor from repo to /home/$username/.icons
+gsettings set org.gnome.desktop.interface cursor-theme Nordic-cursors
+
+
+# Set Wallpaper
+gsettings set org.gnome.desktop.background picture-uri file:///home/$username/root-filetree/images/artwork/wallpaper/aciwnsk876t91.jpg
+
---
+++ ubuntu-flw/ubuntu_post_install.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+  exit 1
+fi
+
+username=$(id -u -n 1000)
+builddir=$(pwd)
+develdir=/home/$username/root-filetree/devel
+tmpinstalldir=/home/$username/tmp
+logfile=$builddir/post-install.log
+
+
+# Update packages list and update system
+apt update
+apt upgrade -y
+
+
+mkdir -p $tmpinstalldir && cd $tmpinstalldir
+
+
+# function to run command as non-root user
+run_as_user() {
+  sudo -u $username bash -c "$1";
+}
+
+print_char_n_times ()
+{
+  str=$1
+  num=$2
+  printf "%0.s$1" $(seq 1 $num)
+  echo
+}
+
+log_step ()
+{
+  local input=$*
+  nbchar=${#input}
+  print_char_n_times "#" $(($nbchar+4))
+  echo "# $* #"
+  print_char_n_times "#" $(($nbchar+4))
+}
+
+
+log_install_step ()
+{
+  log_step $* | tee $logfile
+}
+
+
+# Remove snap "bloats"
+log_install_step "Remove firefox and thunderbird"
+snap remove firefox
+apt purge -y thunderbird*
+apt purge -y gnome-games gnome-{mahjongg,mines,sudoku}
+apt autoremove -y
+
+
+
+
+## Install utilities to facilitate installation ( they are useful anyway )
+log_install_step "Install wget curl  and nala "
+
+apt install -y wget apt-transport-https curl
+wget -qO- https://deb.volian.org/volian/scar.key | gpg --dearmor | dd of=/usr/share/keyrings/volian-archive-scar.gpg  # (sudo wget... if manual)
+echo "deb [signed-by=/usr/share/keyrings/volian-archive-scar.gpg arch=amd64] https://deb.volian.org/volian/ scar main" > /etc/apt/sources.list.d/volian-archive-scar.list
+apt update
+apt install -y nala
+
+
+## Install packages that requires EULA agreement
+
+echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
+apt-get install -y  ttf-mscorefonts-installer
+
+log_install_step "Nala Install of: utilities (CLI) / media / tools / dev tools / desktop environment"
+# Install utiliies CLI
+nala install -y terminator htop bashtop neofetch tldr autojump
+# and utilities media
+nala install -y ubuntu-restricted-extras vlc pulseaudio pavucontrol
+# and tools
+nala install -y keepassxc gpaint gthumb
+# and gaming
+nala install -y steam
+# and developer tools
+nala install -y build-essential cmake git neovim rust-all texlive-latex-extra
+# and desktop environment
+nala install -y gnome-tweaks gnome-shell-extension-manager gnome-shell-extensions
+  # gnome-themes-extra
+
+
+log_install_step " Extra Installs "
+
+# Sublime text 4 + sublime merge
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
+
+
+# Obsidian Markdown Editor
+wget -O obsidian.deb https://github.com/obsidianmd/obsidian-releases/releases/download/v1.0.3/obsidian_1.0.3_amd64.deb
+
+# Brave browser
+curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+# Discord
+wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
+
+# lutris ?
+
+# qwerty-fr keyboard layout
+wget -O qwerty-fr.deb https://github.com/qwerty-fr/qwerty-fr/releases/download/v0.7.2/qwerty-fr_0.7.2_linux.deb
+
+nala update
+nala install -y sublime-text sublime-merge brave-browser
+nala install -y ./obsidian.deb
+nala install -y ./discord.deb
+nala install -y ./qwerty-fr.deb
+
+# # Install itch.io
+# Aller dans ~/Download
+# wget -O itch-setup "https://itch.io/app/download?platform=linux"
+# chmod +x itch-setup && ./itch-setup # attention sudo ou pas...
+
+
+## Desktop environment Configuration
+
+
+## Packages & Programs Configuration
+git config --global user.email "florian.valenza@gmail.com"
+git config --global user.name "Florian Valenza"
+# git config --global credential.helper store
+
+tldr -u # updqte tldr database
+
+
+
+cd $builddir
+rm -rf $tmpinstalldir
+
+## Create root-filetree w/ devel dir
+# TODO. + set DEVEL_DIR, cdd alias, INSTALL_DIR
+cd /home/$username
+run_as_user "mkdir -p root-filetree";
+cd root-filetree
+run_as_user "mkdir -p archives audio devel documents Games images litterature softwares videos";
+cd audio
+run_as_user "mkdir -p music podcast";
+cd ../devel
+run_as_user "mkdir -p book external-src install notes src";
+cd ../images
+run_as_user "mkdir -p artwork charts photos screenshots";
+cd artwork
+run_as_user "mkdir -p wallpaper";
+cd ../../litterature
+run_as_user "mkdir -p book partitions user-manuals";
+
